534203d2a4262e0a1978d583742bd26e0323f25f
DRIZZT2025\drizz <earendil.joan@gmail.com>
2025-12-27 20:49:05 +0100

    Fixed default page and footer

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
     <>
       <Hero />
       <BusinessCategories />
-      <FeaturesPlanet />
-      <LargeTestimonial />
-      <Cta />
+      {/* <FeaturesPlanet />
+      <LargeTestimonial /> 
+      <Cta />*/}
     </>
   );
 }
