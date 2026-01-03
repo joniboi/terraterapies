53bf2665a14fb776daa00bb43e2f1beef7a6972a
DRIZZT2025\drizz <earendil.joan@gmail.com>
2026-01-03 16:39:16 +0100

    removed collaborators

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -62,22 +62,6 @@ export default function Footer({ border = false }: { border?: boolean }) {
                   Mezzanote
                 </Link>
               </li>
-              <li>
-                <Link
-                  className="text-gray-600 transition hover:text-gray-900"
-                  href="#0"
-                >
-                  Wall-52
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="text-gray-600 transition hover:text-gray-900"
-                  href="#0"
-                >
-                  Hotel El Sant Cugat
-                </Link>
-              </li>
             </ul>
           </div>
 
@@ -167,8 +151,9 @@ export default function Footer({ border = false }: { border?: boolean }) {
       </div>
 
       {/* Big text */}
-      <div className="relative -mt-16 h-60 w-full" aria-hidden="true">
-        <div className="pointer-events-none absolute left-1/2 -z-10 -translate-x-1/2 text-center text-[348px] font-bold leading-none before:bg-linear-to-b before:from-gray-200 before:to-gray-100/30 before:to-80% before:bg-clip-text before:text-transparent before:content-['Thai&Bali'] after:absolute after:inset-0 after:bg-gray-300/70 after:bg-clip-text after:text-transparent after:mix-blend-darken after:content-['Thai&Bali'] after:[text-shadow:0_1px_0_white]"></div>
+      {/* ADD pointer-events-none HERE 👇 */}
+      <div className="pointer-events-none relative -mt-16 h-60 w-full" aria-hidden="true">
+        <div className="absolute left-1/2 -z-10 -translate-x-1/2 text-center text-[348px] font-bold leading-none before:bg-linear-to-b before:from-gray-200 before:to-gray-100/30 before:to-80% before:bg-clip-text before:text-transparent before:content-['Thai&Bali'] after:absolute after:inset-0 after:bg-gray-300/70 after:bg-clip-text after:text-transparent after:mix-blend-darken after:content-['Thai&Bali'] after:[text-shadow:0_1px_0_white]"></div>
         {/* Glow */}
         <div
           className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-2/3"
