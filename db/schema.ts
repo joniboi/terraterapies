729bef5144a8de6b2a41fb160c71977867de2a50
DRIZZT2025\drizz <earendil.joan@gmail.com>
2026-07-31 19:15:14 +0200

    change settings to have the address google friendly

--- db/schema.ts
+++ db/schema.ts
@@ -176,10 +176,10 @@ export const siteSettings = pgTable("site_settings", {
   contactPhone: varchar("contact_phone", { length: 50 }).notNull(),
   addressLine1: varchar("address_line1", { length: 255 }).notNull(),
   addressLine2: varchar("address_line2", { length: 255 }).notNull(),
-  streetAddress: varchar("street_address", { length: 255 }).notNull(),
-  addressLocality: varchar("address_locality", { length: 100 }).notNull(), // City
+  streetAddress: varchar("street_address", { length: 255 }),
+  addressLocality: varchar("address_locality", { length: 100 }), // City
   addressRegion: varchar("address_region", { length: 100 }), // Province/State
-  postalCode: varchar("postal_code", { length: 20 }).notNull(),
+  postalCode: varchar("postal_code", { length: 20 }),
   addressCountry: varchar("address_country", { length: 2 })
     .default("ES")
     .notNull(),
